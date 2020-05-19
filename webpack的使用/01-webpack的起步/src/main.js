57ed54d4c21de9b0542ac0e32f894ad73c28eded
easonyes <2234148120@qq.com>
2020-05-20 01:51:15 +0800

    less配置

--- webpack的使用/01-webpack的起步/src/main.js
+++ webpack的使用/01-webpack的起步/src/main.js
@@ -5,3 +5,8 @@ console.log(mul(20, 30))
 
 // 依赖css文件
 require('./css/normal.css')
+
+// 引入less文件
+require('./css/special.less')
+
+document.writeln('<h2>嘻嘻</h2>')
--- webpack的使用/01-webpack的起步/webpack.config.js
+++ webpack的使用/01-webpack的起步/webpack.config.js
@@ -6,5 +6,23 @@ module.exports = {
     // 动态获取路径
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.less$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "less-loader" // compiles Less to CSS
+        }]
+      }
+    ]
   }
 }
