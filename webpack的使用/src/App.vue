6115779dc82834c58298f2978c296566bc4aac41
easonyes <2234148120@qq.com>
2020-05-23 17:18:52 +0800

    更新文件夹路径

---
+++ webpack的使用/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{name}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'ashdihasiodhioa'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ webpack的使用/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    // 动态获取路径
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: 'dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.less$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "less-loader" // compiles Less to CSS
+        }]
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'img/[name].[hash:8].[ext]'
+            },
+          },
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      },
+      {
+        test: /\.vue$/,
+        use: {
+          loader: 'vue-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    // 别名
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+}
